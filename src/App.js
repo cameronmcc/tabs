da2f0cb038f7666fc6d4827d8cac8ee3f74011a1
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-08-11 20:38:54 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setjobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setjobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+  return <h2>jobs</h2>;
+}
+
+export default App;
