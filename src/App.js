80659fe4c65c19095f9b70e34548cc3e7baaffa5
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-08-12 21:01:20 -0600

    Added button hover animation

--- src/App.js
+++ src/App.js
@@ -38,7 +38,11 @@ function App() {
           <div className='btn-container'>
             {jobs.map((item, index) => {
               return (
-                <button key={item.id} onClick={() => setValue(index)}>
+                <button
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className={`job-btn ${index === value && "active-btn"}`}
+                >
                   {item.company}
                 </button>
               );
