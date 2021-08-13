ea3853b68f343503147a1cb6939c10decacc90d1
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-08-12 21:11:17 -0600

    build folder added for deployment

---
+++ build/precache-manifest.05fad3f65276314518eabef87828493b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a5c5e5de111347d9f28255b53586ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce65242824d02f698add",
+    "url": "/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce65242824d02f698add",
+    "url": "/static/js/main.5ee7983e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
